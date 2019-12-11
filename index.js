ba67fb08b034889bbce6bbb9704b0c7ce980d0b7
Shivam Mundhra <smundhra4478@gmail.com>
2019-12-11 23:17:59 +0530

    functions shifted into modules

--- index.js
+++ index.js
@@ -2,29 +2,7 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
-// BLOCKING,SYNCHRONUS WAY
-// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-// console.log(textIn);
-// console.log(typeof textIn);
-
-// const textOut = `This is what we know about avocado: ${textIn} \n create on ${Date.now()}`;
-// fs.writeFileSync("./txt/output.txt", textOut);
-
-//NON blocking way
-
-// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-//     console.log(data2);
-//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-//       console.log(data3);
-//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", err => {
-//         console.log("your file has been written");
-//       });
-//     });
-//   });
-// });
-
-// console.log("will read the file");
+const replaceTemplate = require("./modules/replaceTemplate");
 
 ////////////////////////////////////
 //SERVER
@@ -44,20 +22,6 @@ const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
   "utf-8"
 );
-
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%ID%}/g, product.id);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
